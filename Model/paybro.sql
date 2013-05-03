a68662eea5c15cd580e3ea20bcac4f71e67bd023
afrishal <afrishal@gmail.com>
2013-05-03 16:31:58 +0700

    file sql

---
+++ Model/paybro.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 03, 2013 at 04:30 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `paybro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `username` varchar(20) NOT NULL,
+  `password` char(32) NOT NULL,
+  `roleID` int(11) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `roleID` (`roleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `roleID` int(11) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(10) NOT NULL,
+  PRIMARY KEY (`roleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `transactionID` char(10) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `amount` int(11) NOT NULL,
+  `payer` varchar(20) NOT NULL,
+  `receiver` varchar(20) NOT NULL,
+  `typeID` int(11) NOT NULL,
+  PRIMARY KEY (`transactionID`),
+  KEY `typeID` (`typeID`),
+  KEY `payer` (`payer`),
+  KEY `receiver` (`receiver`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_type`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction_type` (
+  `typeID` int(11) NOT NULL,
+  `transactionName` varchar(20) NOT NULL,
+  PRIMARY KEY (`typeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `placeOfBirth` varchar(30) NOT NULL,
+  `dateOfBirth` date NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `province` varchar(30) NOT NULL,
+  `postalCode` varchar(10) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `balance` int(11) NOT NULL,
+  PRIMARY KEY (`username`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_ibfk_1` FOREIGN KEY (`roleID`) REFERENCES `role` (`roleID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`typeID`) REFERENCES `transaction_type` (`typeID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`payer`) REFERENCES `user` (`username`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `user` (`username`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`username`) REFERENCES `account` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
